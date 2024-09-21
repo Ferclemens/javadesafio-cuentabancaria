eb8450e84aab6f7dd2df7fe0fb3a3daeea24dbcf
Ferclemens <foclemens@gmail.com>
2024-09-21 14:08:51 -0300

    se agrego Descripcion de cuenta

---
+++ src/Cuenta.java
@@ -0,0 +1,13 @@
+public class Cuenta {
+    public static void main(String[] args) {
+        String nombre = "Don Ramon";
+        String tipoDeCuenta = "Corriente";
+        double saldo = 1599.99;
+
+        System.out.println("********************************");
+        System.out.println("Nombre del cliente: "+ nombre);
+        System.out.println("Tipo de cuenta: " + tipoDeCuenta);
+        System.out.println("Saldo disponible: u$s "+ saldo);
+        System.out.println("********************************");
+    }
+}
